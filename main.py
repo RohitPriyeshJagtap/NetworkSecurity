4653614fd65e3cdaa7d4a040a240a1beebaa7ff3
Rohit Priyesh Jagtap <jagtaprohit005011999@gmail.com>
2025-07-27 19:32:11 +0530

    Implement model training functionality and related configurations

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import sys
 from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.entity.config_entity import ModelTrainerConfig
+from networksecurity.components.model_trainer import ModelTrainer
 
 if __name__ == "__main__":
     try:
@@ -29,6 +31,12 @@ if __name__ == "__main__":
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         logging.info("Data transformation completed")
         print(data_transformation_artifact)
+
+        logging.info("Model Training Started")
+        model_trainer_config = ModelTrainerConfig(training_pipeline_config)
+        model_trainer = ModelTrainer(model_trainer_config, data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info("Model Training Completed")
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
